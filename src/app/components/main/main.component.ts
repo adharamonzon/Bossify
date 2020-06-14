9211bf7d3a66cbdd54794d5ccf769361c189e321
adharamonzon <adharamonzon@gmail.com>
2020-06-14 18:27:46 +0200

    add new bands array, functionaly add bands to the list

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import bands from '../bands';
+import moreBands from '../more-bands';
 
 @Component({
   selector: 'app-main',
@@ -9,27 +10,53 @@ import bands from '../bands';
 })
 export class MainComponent implements OnInit {
   public Mybands: Array<object>;
+  public moreBands: Array<object>;
   public values: any;
-  public filterBands: Array<object>;
+  public filterBands: Array<any>;
   constructor(private router: Router) {
     this.Mybands = bands;
+    this.moreBands = moreBands;
     this.filterBands = [];
   }
 
-  ngOnInit(): void {} //se ejecuta despues de que el componente se haya creado correctamente
+  ngOnInit(): void {}
+  //onSelect //Angular routing. Función para poder seleccionar una banda y acceder a su detalle.
   onSelect() {
     for (const band of bands) {
       return this.router.navigate(['/bandDetail', band.id]);
     }
   }
+  //filterByName //filtrado por nombre: Obtiene el valor del imput, a través de click en el botón o dejar el foco del imput, se itera para tener un nuevo array con las bandas filtradas
   filterByName(event: any) {
     this.values = event.currentTarget.value.toLowerCase();
 
     for (const band of bands) {
-      if (band.name.includes(this.values)) {
+      if (this.filterBands.length <= 0 && band.name.includes(this.values)) {
+        this.filterBands.push(band);
+      } else if (band.name.includes(this.values) && band.name !== this.filterBands['name']) {
         this.filterBands.push(band);
       }
-      console.log(this.filterBands);
     }
+    console.log(this.filterBands);
+  }
+  //showList // función para desplegar el menú de nuevos grupos
+  showList() {
+    const list = document.querySelector('.add-band__list');
+    list.classList.toggle('show');
+  }
+  //  addBand // función para encontrar la nueva banda seleccionada y añadirla al array con todas las bandas, llama a la funcion de eliminar la banda de la lista de añadir
+  addBand($event) {
+    let newBand;
+    const newBandId = parseInt($event.toElement.id);
+    moreBands.findIndex((band) => {
+      if (band.id === newBandId) return (newBand = band);
+    });
+    this.Mybands.push(newBand);
+    this.removeBandList(newBand);
+  }
+  //removeBandList // esta función elimina la banda de la lista de añadir y evita que se añada la banda más de una vez
+  removeBandList(newBand) {
+    const bandPossition = moreBands.indexOf(newBand);
+    moreBands.splice(bandPossition, 1);
   }
 }
---
+++ src/app/components/more-bands.ts
@@ -0,0 +1,39 @@
+const moreBands = [
+  {
+    id: 10,
+    name: 'Creedence',
+    description: 'Creedence Clearwater Revival (a veces estilizado como CCR) fue una banda estadounidense de rock, popular a fines de la década de 1960 y comienzos de los 70. Considerada una de las mejores bandas de la historia, es una de las más populares e influyentes. El estilo musical de Creedence combinó el rock and roll con otros géneros tales como el blues, el góspel, el country y el swamp rock. La denominación genérica roots rock, se refiere a la música inspirada en las raíces tradicionales de la música norteamericana, no existía en la época en que se formó CCR, pero hoy en día sería perfectamente aplicable a su estética. Por su estilo de música tan peculiar, se les considera los reyes de la carretera, pues su música te lleva a imaginarte que estás conduciendo por una autopista. En sus primeros álbumes tenía un sonido bastante psicodélico y considerados como la banda precursora del Grunge.',
+    bandMembers: ['John Fogerty', 'Tom Fogerty', 'Stu Cook', 'Doug Clifford'],
+    image: '../../.././assets/images/creedence.jpg',
+  },
+  {
+    id: 11,
+    name: 'Black Sabbath',
+    description: 'Black Sabbath es una banda británica de heavy metal formada en 1968 en Birmingham. La agrupación incorporó desde sus inicios letras sobre ocultismo y terror con guitarras afinadas de modo más grave y consiguió varios discos de oro y platino en la década de 1970. Son los pioneros del heavy metal. Han vendido más de 75 millones de discos en todo el mundo, incluidos 15 millones de copias sólo en los Estados Unidos.',
+    bandMembers: ['Tony Iommi', 'Ozzy Osbourne', 'Geezer Butler', 'Bill Ward'],
+    image: '../../.././assets/images/black-sabbath.jpg',
+  },
+  {
+    id: 12,
+    name: 'Fleetwood Mac',
+    description: 'Fleetwood Mac es una banda de rock nacida en 1967 en Londres, que a lo largo de su carrera ha sufrido constantes cambios en su alineación y ha tenido diversos períodos de popularidad. El apellido de John McVie combinado con el de Mick Fleetwood fue escogido para titular la banda y junto a él son los únicos músicos que han estado en toda la carrera de la agrupación inglesa. En conjunto con sus primeros trabajos, se situaron como una de las bandas más importantes del boom del blues británico de fines de los sesenta y principios de los setenta. Hasta el día de hoy Fleetwood Mac es reconocida como una de las bandas más exitosas del mundo con más de 100 millones de copias vendidas, cuyo disco Rumours se posiciona dentro de los top 10 de la lista de los álbumes más vendidos de todos los tiempos.',
+    bandMembers: ['Mick Fleetwood', 'John McVie', 'Stevie Nicks', 'Christine McVie', 'Lindsey Buckingham'],
+    image: '../../.././assets/images/fleetwood-mac.jpg',
+  },
+  {
+    id: 13,
+    name: 'The Who',
+    description: 'The Who es una banda británica de rock considerada un icono de la música del siglo XX, formada en 1962 con el nombre de The Detours que posteriormente cambió a The Who tras la incorporación del baterista Keith Moon. De esa manera el grupo, potenció su carrera escalando a los primeros puestos del ambiente del rock inglés. Fue conocida por sus energéticas presentaciones en vivo las cuales incluían la destrucción de sus instrumentos. The Who ha vendido alrededor de 100 millones de discos en todo el mundo, obteniendo 18 discos de oro, 12 de platino y 5 de multi-platino solo en los Estados Unidos.',
+    bandMembers: ['Roger Daltrey', 'Pete Townshend', 'John Entwistle', 'Keith Moon', 'Kenney Jones'],
+    image: '../../.././assets/images/who.jpg',
+  },
+  {
+    id: 14,
+    name: 'Tom Petty & the HeartBreakers',
+    description: 'Con The Heartbreakers, Petty grabó y publicó varios sencillos de éxito. A lo largo de su carrera, Petty vendió más de ochenta millones de discos a nivel mundial, convirtiéndolo en uno de los artistas musicales con mayores ventas. Petty recibió diez nominaciones a los Premios Grammy desde 1981.',
+    bandMembers: ['Tom Petty', 'Mike Campbell', 'Benmont Tench', 'Ron Blair', 'Scott Thurston', 'Steve Ferrone'],
+    image: '../../.././assets/images/who.jpg',
+  },
+];
+
+export default moreBands;
